457cf78cbd2aae131893a0a0d3acb27d9f86500e
Olivier Leduc <leduc.o@gmail.com>
2022-07-05 23:05:02 -0400

    feat: named workspaces (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,42 @@
 mod cli;
-mod load;
+mod config;
+mod tmux;
+
+use std::{fs::read_dir, path::Path};
 
 use crate::cli::Args;
-use crate::load::Config;
-use anyhow::{Context, Result};
+use crate::config::Config;
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use load::BeforeCommand;
-use tmux_interface::{AttachSession, NewSession, TmuxCommand};
+use tmux::Session;
 
-struct Session {
-    name: Option<String>,
-    dir: String,
-    windows: Vec<Window>,
+fn file_exists(path: String) -> bool {
+    Path::new(&path).exists()
 }
 
-impl Session {
-    pub fn from_config(config: Config) -> Self {
-        Session {
-            name: config.session_name,
-            dir: config.start_directory.unwrap_or_else(|| String::from(".")),
-            windows: config
-                .windows
-                .into_iter()
-                .map(Window::from_config)
-                .collect(),
+fn find_workspace_file(name: String) -> Result<String> {
+    let workspace_root = Path::new(&dirs::home_dir().unwrap()).join(".tmup");
+    let paths = read_dir(workspace_root)?;
+    for path in paths.flatten() {
+        let file = path.path();
+        let stem = file.file_stem().unwrap().to_str().unwrap();
+        if stem == name {
+            return Ok(path.path().to_str().unwrap().to_string());
         }
     }
-
-    pub fn build(&self) -> Result<()> {
-        let root = &self.dir;
-        let mut session = NewSession::new();
-        session.detached();
-        session.start_directory(root);
-
-        if let Some(name) = &self.name {
-            session.session_name(name);
-        }
-
-        for (i, window) in self.windows.iter().enumerate() {
-            if i == 0 {
-                if let Some(name) = &window.name {
-                    session.window_name(name);
-                }
-
-                session.output()?;
-
-                if let Some(cmds) = &window.cmds {
-                    send_command(cmds.to_vec())?;
-                }
-            } else {
-                window.build(root.to_string())?;
-            }
-        }
-
-        AttachSession::new()
-            .output()
-            .context("Failed to attach to session")?;
-
-        Ok(())
-    }
-}
-
-struct Window {
-    name: Option<String>,
-    dir: Option<String>,
-    cmds: Option<Vec<String>>,
-}
-
-impl Window {
-    pub fn from_config(config: load::Window) -> Self {
-        Window {
-            name: Some(config.name),
-            dir: config.start_directory,
-            cmds: match config.before_command {
-                None => None,
-                Some(BeforeCommand::One(cmd)) => Some(vec![cmd]),
-                Some(BeforeCommand::Many(cmds)) => Some(cmds),
-            },
-        }
-    }
-
-    pub fn build(&self, root: String) -> Result<()> {
-        let mut cmd = TmuxCommand::new().new_window();
-
-        if let Some(name) = &self.name {
-            cmd.window_name(name);
-        }
-
-        if let Some(dir) = &self.dir {
-            cmd.start_directory(dir);
-        } else {
-            cmd.start_directory(root);
-        }
-
-        cmd.output()?;
-
-        if let Some(cmds) = &self.cmds {
-            send_command(cmds.to_vec())?;
-        }
-
-        Ok(())
-    }
-}
-
-pub fn send_command(cmds: Vec<String>) -> Result<()> {
-    for cmd in cmds {
-        TmuxCommand::new().send_keys().key(cmd + "\n").output()?;
-    }
-    Ok(())
+    Err(anyhow!("Workspace not found"))
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    let config = Config::from_file(args.file).context("Failed to read config from file")?;
+    println!("{:?}", args);
+    let file = match file_exists(args.file.clone()) {
+        true => args.file,
+        false => find_workspace_file(args.file)?,
+    };
+
+    println!("{}", file);
+    let config = Config::from_file(file).context("Failed to read config from file")?;
     let session = Session::from_config(config);
     session.build()?;
 
